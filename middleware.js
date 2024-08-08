5fe54a0b8f3178e9122d5ebf35c7d7f7392cef77
Henka No Jin <jose.mariovalentino@gmail.com>
2024-08-08 08:42:16 +0700

    Fix[HNJ]Fixing Middleware Admin Page Bug

--- middleware.js
+++ middleware.js
@@ -2,26 +2,19 @@ import { NextResponse } from 'next/server';
 
 export function middleware(request) {
   const token = request.cookies.get('token');
-  const role = request.cookies.get('role');
+  const isAdmin = request.cookies.get('isAdmin');
   const motorId = request.cookies.get('motorId');
-
-  console.log('role:', role);
-  const admin = role === 'admin';
-  console.log('role:', admin);
 
   if (!token) {
     const returnUrl = request.nextUrl.pathname;
     return NextResponse.redirect(new URL(`/login?returnUrl=${encodeURIComponent(returnUrl)}`, request.url));
   }
 
-  // Handle access to the /admin page
-  // if (request.nextUrl.pathname === '/admin') {
-  //   // if (admin !== true) {
-  //   //   return NextResponse.redirect(new URL('/', request.url));
-  //   // } else {
-  //   //   return NextResponse.redirect(new URL('/admin', request.url));
-  //   // }
-  // }
+  if (request.nextUrl.pathname === '/admin') {
+    if (!isAdmin) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  }
 
   // Redirect to specific motor form if motorId is present
   if (request.nextUrl.pathname === '/form') {
