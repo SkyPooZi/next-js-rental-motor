996e6befa534deaee578bc6800e81721d22f07c5
Henka No Jin <jose.mariovalentino@gmail.com>
2024-08-18 23:23:56 +0700

    Style[HNJ]Responsive setting side navbar

--- components/ui/defaultSpeedDial.jsx
+++ components/ui/defaultSpeedDial.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-
 import {
     IconButton,
     SpeedDial,
@@ -16,42 +15,47 @@ import {
     PlusIcon,
 } from "@heroicons/react/24/outline";
 
-import Profile from "@/components/sub/profile";
-import Point from "@/components/sub/point";
-import History from "@/components/sub/history";
-import Terms from "@/components/sub/terms";
-
 export function DefaultSpeedDial({ activeComponent, handleButtonClick }) {
+    const [open, setOpen] = useState(false);
+
+    const toggleOpen = () => {
+        setOpen(!open);
+    };
+
+    const handleActionClick = (component) => {
+        handleButtonClick(component);
+        setOpen(false); // Close the SpeedDial after an action is clicked
+    };
 
     return (
         <div className="relative h-80 w-full">
             <div className="absolute bottom-0 right-0">
-                <SpeedDial>
+                <SpeedDial open={open} onToggle={toggleOpen}>
                     <SpeedDialHandler>
-                        <IconButton size="lg" className="rounded-full bg-[#FF4D33]">
-                            <PlusIcon className="h-5 w-5 transition-transform group-hover:rotate-45" />
+                        <IconButton size="lg" className="rounded-full bg-[#FF4D33]" onClick={toggleOpen}>
+                            <PlusIcon className={`h-5 w-5 transition-transform ${open ? 'rotate-45' : ''}`} />
                         </IconButton>
                     </SpeedDialHandler>
                     <SpeedDialContent>
-                        <SpeedDialAction className={`h-16 w-16 ${activeComponent === 'profile' ? 'bg-[#FF4D30] text-white' : ''}`} onClick={() => handleButtonClick('profile')}>
+                        <SpeedDialAction className={`h-16 w-16 ${activeComponent === 'profile' ? 'bg-[#FF4D30] text-white' : ''}`} onClick={() => handleActionClick('profile')}>
                             <CgProfile className="h-5 w-5" />
                             <Typography color="blue-gray" className={`text-xs font-normal ${activeComponent === 'profile' ? 'text-white' : ''}`}>
                                 Profil
                             </Typography>
                         </SpeedDialAction>
-                        <SpeedDialAction className={`h-16 w-16 ${activeComponent === 'point' ? 'bg-[#FF4D30] text-white' : ''}`} onClick={() => handleButtonClick('point')}>
+                        <SpeedDialAction className={`h-16 w-16 ${activeComponent === 'point' ? 'bg-[#FF4D30] text-white' : ''}`} onClick={() => handleActionClick('point')}>
                             <AiOutlineDollarCircle className="h-5 w-5" />
                             <Typography color="blue-gray" className={`text-xs font-normal ${activeComponent === 'point' ? 'text-white' : ''}`}>
                                 Poin
                             </Typography>
                         </SpeedDialAction>
-                        <SpeedDialAction className={`h-16 w-16 ${activeComponent === 'history' ? 'bg-[#FF4D30] text-white' : ''}`} onClick={() => handleButtonClick('history')}>
+                        <SpeedDialAction className={`h-16 w-16 ${activeComponent === 'history' ? 'bg-[#FF4D30] text-white' : ''}`} onClick={() => handleActionClick('history')}>
                             <AiOutlineHistory className="h-5 w-5" />
                             <Typography color="blue-gray" className={`text-xs font-normal ${activeComponent === 'history' ? 'text-white' : ''}`}>
                                 Riwayat
                             </Typography>
                         </SpeedDialAction>
-                        <SpeedDialAction className={`h-16 w-16 ${activeComponent === 'terms' ? 'bg-[#FF4D30] text-white' : ''}`} onClick={() => handleButtonClick('terms')}>
+                        <SpeedDialAction className={`h-16 w-16 ${activeComponent === 'terms' ? 'bg-[#FF4D30] text-white' : ''}`} onClick={() => handleActionClick('terms')}>
                             <FiInfo className="h-5 w-5" />
                             <Typography color="blue-gray" className={`text-xs font-normal ${activeComponent === 'terms' ? 'text-white' : ''}`}>
                                 Kebijakan
